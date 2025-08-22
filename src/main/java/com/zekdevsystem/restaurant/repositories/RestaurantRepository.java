4eeae601a5fd37390b633b714ff2d731dcb0c2bf
MyDevSystem <rawaha.coder@gmail.com>
2025-08-22 12:06:06 +0100

    create Restaurant repository

---
+++ src/main/java/com/zekdevsystem/restaurant/repositories/RestaurantRepository.java
@@ -0,0 +1,9 @@
+package com.zekdevsystem.restaurant.repositories;
+
+import com.zekdevsystem.restaurant.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+}
